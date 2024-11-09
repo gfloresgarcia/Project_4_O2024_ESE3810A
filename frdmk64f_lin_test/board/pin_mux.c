249600955d6f61524889467e4d3c5a1089dce528
CeicGitHub <cesar.inda@iteso.mx>
2024-11-09 12:55:42 -0600

    Enable:Ports/Clocks/Pins/BitsRegister_Added

--- frdmk64f_lin_test/board/pin_mux.c
+++ frdmk64f_lin_test/board/pin_mux.c
@@ -1,60 +1,17 @@
-/*
- * The Clear BSD License
- * Copyright (c) 2016, Freescale Semiconductor, Inc.
- * Copyright 2016-2017 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided
- * that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- * TEXT BELOW IS USED AS SETTING FOR TOOLS *************************************
-!!GlobalInfo
-product: Pins v3.0
-processor: MK64FN1M0xxx12
-package_id: MK64FN1M0VLL12
-mcu_data: ksdk2_0
-processor_version: 2.0.0
- * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
- */
-
+/*This file contain the PINS and Clocks*/
 #include "fsl_common.h"
 #include "fsl_port.h"
 #include "pin_mux.h"
-
 
 #define PIN14_IDX                       14u   /*!< Pin number for pin 14 in a port */
 #define PIN15_IDX                       15u   /*!< Pin number for pin 15 in a port */
 #define PIN16_IDX                       16u   /*!< Pin number for pin 16 in a port */
 #define PIN17_IDX                       17u   /*!< Pin number for pin 17 in a port */
+#define PIN03_IDX                       03u
+#define PIN04_IDX						04u
 #define SOPT5_UART0TXSRC_UART_TX      0x00u   /*!< UART 0 transmit data source select: UART0_TX pin */
 
-/*
+/*Use the next text for configure the tools
  * TEXT BELOW IS USED AS SETTING FOR TOOLS *************************************
 BOARD_InitPins:
 - options: {callFromInitBoot: 'true', coreID: core0, enableClock: 'true'}
@@ -71,25 +28,64 @@ BOARD_InitPins:
  *
  *END**************************************************************************/
 void BOARD_InitPins(void) {
+
+  CLOCK_EnableClock(kCLOCK_PortA);                           /* Port A Clock Gate Control: Clock enabled */
   CLOCK_EnableClock(kCLOCK_PortB);                           /* Port B Clock Gate Control: Clock enabled */
   CLOCK_EnableClock(kCLOCK_PortC);                           /* Port C Clock Gate Control: Clock enabled */
+  CLOCK_EnableClock(kCLOCK_PortD);                           /* Port D Clock Gate Control: Clock enabled */
+  CLOCK_EnableClock(kCLOCK_PortE);                           /* Port E Clock Gate Control: Clock enabled */
 
+  //Clocks_UART's
+  CLOCK_EnableClock(kCLOCK_Uart0);
+  CLOCK_EnableClock(kCLOCK_Uart1);
+  CLOCK_EnableClock(kCLOCK_Uart3);
+  CLOCK_EnableClock(kCLOCK_Uart3);
+  CLOCK_EnableClock(kCLOCK_Uart4);
+  CLOCK_EnableClock(kCLOCK_Uart5);
+
+  /*UART 0 pins_config*/
   PORT_SetPinMux(PORTB, PIN16_IDX, kPORT_MuxAlt3);           /* PORTB16 (pin 62) is configured as UART0_RX */
   PORT_SetPinMux(PORTB, PIN17_IDX, kPORT_MuxAlt3);           /* PORTB17 (pin 63) is configured as UART0_TX */
-  SIM->SOPT5 = ((SIM->SOPT5 &
-    (~(SIM_SOPT5_UART0TXSRC_MASK)))                          /* Mask bits to zero which are setting */
-      | SIM_SOPT5_UART0TXSRC(SOPT5_UART0TXSRC_UART_TX)       /* UART 0 transmit data source select: UART0_TX pin */
-    );
 
-  /*UART 3 pins config*/
+ //UART 1 Pins_Config
+  PORT_SetPinMux(PORTC, PIN03_IDX, kPORT_MuxAlt3);           /* PORTC03 (pin 73) is configured as UART0_RX */
+  PORT_SetPinMux(PORTC, PIN04_IDX, kPORT_MuxAlt3);           /* PORTC04 (pin 76) is configured as UART0_TX */
+  /*UART 3 pins_config*/
   PORT_SetPinMux(PORTC, PIN16_IDX, kPORT_MuxAlt3);           /* PORTB14 (pin 90) is configured as UART3_RX */
   PORT_SetPinMux(PORTC, PIN17_IDX, kPORT_MuxAlt3);           /* PORTB15 (pin 91) is configured as UART3_TX */
-  /*UART 4 pins config*/
+
+  /*UART 4 pins_config*/
   PORT_SetPinMux(PORTC, PIN14_IDX, kPORT_MuxAlt3);           /* PORTB16 (pin 86) is configured as UART4_RX */
   PORT_SetPinMux(PORTC, PIN15_IDX, kPORT_MuxAlt3);           /* PORTB17 (pin 87) is configured as UART4_TX */
+
+  //Led_Pins
+  PORT_SetPinMux(PORTB, 22U, kPORT_MuxAsGpio);				 /* PORTB22 (pin 68) is configured as PTB22 */
+  PORT_SetPinMux(PORTB, 21U, kPORT_MuxAsGpio);				 /* PORTB21 (pin 71) is configured as PTB21 */
+  PORT_SetPinMux(PORTE, 26U, kPORT_MuxAsGpio);				 /* PORTE26 (pin 73) is configured as PTE26 */
+
+
+ //Mask Bits To Zero (Setting) & UART 0 Transmit Data Source Select: UART0_TX pin
+  SIM->SOPT5 = ((SIM->SOPT5 & (~(SIM_SOPT5_UART0TXSRC_MASK))) | SIM_SOPT5_UART0TXSRC(SOPT5_UART0TXSRC_UART_TX));
+
+
+  //SW(Buttons);
+  const port_pin_config_t buttons_config =
+  {
+		  	  	  	  	  	 kPORT_PullUp, 			     //Internal pull-up resistor is enabled
+							 kPORT_OpenDrainDisable,     //Open drain is disabled
+							 kPORT_FastSlewRate,		 //Fast slew rate is configured
+							 kPORT_MuxAsGpio,            //Pin is configured as PTA4
+							 kPORT_PassiveFilterDisable, //Passive filter is disabled
+							 kPORT_HighDriveStrength,    //High drive strength is configured
+							 kPORT_UnlockRegister        //Pin Control Register fields [15:0] are not locked
+  };
+
+  PORT_SetPinConfig(PORTA, 4U, &buttons_config);			 /* PORTA4 (pin 38) is configured as PTA4 */
+  PORT_SetPinConfig(PORTC, 6U, &buttons_config);			 /* PORTC6 (pin 78) is configured as PTC6 */
 
 }
 
 /*******************************************************************************
  * EOF
  ******************************************************************************/
+
--- frdmk64f_lin_test/source/driver_lin/lin1d3_driver_Cfg.h
+++ frdmk64f_lin_test/source/driver_lin/lin1d3_driver_Cfg.h
@@ -1,14 +1,47 @@
-/*
- * lin1d3_driver_Cfg.h
- *
- *  Created on: Sep 14, 2018
- *      Author: Nico
- */
-
 #ifndef LIN1D3_DRIVER_CFG_H_
 #define LIN1D3_DRIVER_CFG_H_
 
-#define lin1d3_max_supported_messages_per_node_cfg_d	(16)
+#define lin1d3_max_supported_messages_per_node_cfg_d (16)
 
+
+//Definitions_to bits/pins in register
+typedef enum
+{
+	BIT0,
+	BIT1,
+	BIT2,
+	BIT3,
+	BIT4,
+	BIT5,
+	BIT6,
+	BIT7,
+	BIT8,
+	BIT9,
+	BIT10,
+	BIT11,
+	BIT12,
+	BIT13,
+	BIT14,
+	BIT15,
+	BIT16,
+	BIT17,
+	BIT18,
+	BIT19,
+	BIT20,
+	BIT21,
+	BIT22,
+	BIT23,
+	BIT24,
+	BIT25,
+	BIT26,
+	BIT27,
+	BIT28,
+	BIT29,
+	BIT30,
+	BIT31
+} BitsType;
+
+typedef enum {FALSE, TRUE} boolean_t;
+typedef enum {BIT_OFF, BIT_ON} BIT_ON_OFF_Type; //Bits for turn_on/off any bit
 
 #endif /* LIN1D3_DRIVER_CFG_H_ */
